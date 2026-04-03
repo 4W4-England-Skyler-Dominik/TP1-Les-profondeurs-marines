23060c8dc3e1101f5521532b466bd5a95eb0f787
Skyler-Dominik-England <150696786+ItsSpo0ks@users.noreply.github.com>
2026-04-02 20:02:38 -0400

    Site web classique

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-// Gestion menu et bouton burger.
+/* ---------------------------------------
+         Gestion du menu burger
+----------------------------------------*/
 const burger = document.getElementById("burger");
 const liens = document.getElementById("liens");
 
 burger.addEventListener("click", () => {
+  // Active et désactive la classe 'active' pour montrer ou masquer le menu.
   liens.classList.toggle("active");
+});
+
+/* ---------------------------------------
+         Gestion des carousels
+--------------------------------------- */
+document.querySelectorAll(".carousel").forEach((carrousel) => {
+  const piste = carrousel.querySelector(".carousel-piste");
+  const image = carrousel.querySelectorAll(".image");
+  const boutonSuivant = carrousel.querySelector(".suivant");
+  const boutonPrecedent = carrousel.querySelector(".precedent");
+
+  let indice = 0; // Index de la diapositive actuelle.
+
+  // Fonction pour déplacer la piste et afficher la bonne diapositive.
+  function mettreAJourCarrousel() {
+    piste.style.transform = `translateX(-${indice * 100}%)`;
+  }
+
+  // Bouton suivant.
+  boutonSuivant.addEventListener("click", () => {
+    indice = (indice + 1) % image.length; // boucle à zéro si fin.
+    mettreAJourCarrousel();
+  });
+
+  // Bouton précédent.
+  boutonPrecedent.addEventListener("click", () => {
+    indice = (indice - 1 + image.length) % image.length; // boucle à la fin si début.
+    mettreAJourCarrousel();
+  });
 });
